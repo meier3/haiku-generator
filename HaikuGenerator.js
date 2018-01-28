338b8007d1f0912774f562368ef182620d4bdb72
brownl1 <brownl1@stolaf.edu>
2018-01-28 12:54:20 -0600

    killin it

--- HaikuGenerator.js
+++ HaikuGenerator.js
@@ -90,7 +90,7 @@ function makeline(syl,max) {
 function printLine(line){
   let str="";
   for(let i = 0; i < line.length; i++)
-    str+= line[i] + 'ensp;';
+    str+= line[i] + "  ";
   return str;
 }
 
