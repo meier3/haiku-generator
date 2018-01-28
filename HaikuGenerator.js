8f0834266aed5b3fb0db65bb187dfeba9ae95b70
brownl1 <brownl1@stolaf.edu>
2018-01-28 03:16:39 -0600

    *

--- HaikuGenerator.js
+++ HaikuGenerator.js
@@ -2,88 +2,71 @@ let syllable = require('syllable');
 const words = require('categorized-words');
 /*
 
-N	noun	47004
-V	verb	31232
-A	adjective	14903
-I	interjection	188
-C	conjunction/preposition	139
-P	pronoun	78
-S	spoken contraction	9
-Total of all classes	93553
+N	noun
+V	verb
+A	adjective
+I	interjection
+C	conjunction/preposition
+P	pronoun
+S	spoken contraction
+Total of all classes
 
 */
 
 let line1,line2,line3; //lines in haiku
-let word1,word2,word3,word4,word5,word6,word7; //words in haiku
-let currentType,nextType,nextType2; //integer values representing pos
+let l1 = [];
+let l2 = [];
+let l3 = [];
+let word;
+let sylNum = 0;
+let currentType; //integer values representing pos
 let randNext; //randomly generated number to determine next pos
 let posArr = [words.N,words.A,words.I,words.C,words.P,words.V];
-let posNext = [[5,5,3,3],[0,0,0,0],[0,1,3,4],[0,1,4,5],[0,1,3,5],[0,0,4,4]]; //pos of speech that can follow each pos in posArr
-let posNumArr = [47004,14903,188,139,78,31232]; //number of words for each pos in posArr
-
-//generates pos for line1
-currentType = getRandomInt(0,4);
-//console.log(currentType);
-randNext = getRandomInt(0,3);
-nextType = posNext[currentType][randNext];
-//console.log(nextType);
-
+let posNext = [[5,5,3,3],[0,0,0,0],[0,4,3,1],[0,5,4,1],[0,5,3,1],[0,4,0,4]]; //pos of speech that can follow each pos in posArr
+let posNumArr = [words.N.length,words.A.length,words.I.length,words.C.length,words.P.length,words.V.length]; //number of words for each pos in posArr
 let sylCt = 0; //syllable count
 
-//generates words for line1
-while(sylCt !=5){
-    word1 = posArr[currentType][getRandomInt(0,posNumArr[currentType])];
-    word2 = posArr[nextType][getRandomInt(0,posNumArr[nextType])];
-    line1 = word1+" "+word2;
-    sylCt = syllable(line1);
+
+//generates pos for line1
+/////////////////////////TO DO: FIX THOSE YOUNG LONG SYLLABLE WORDS
+currentType = getRandomInt(0,4);
+l1.push(posArr[currentType][getRandomInt(0,posNumArr[currentType])]);
+sylNum+=syllable(l1[0]);
+
+l1 = l1.concat(makeline(5,3));
+l2 = makeline(7,3);
+l3 = makeline(5,1);
+
+console.log(printLine(l1));
+console.log(printLine(l2));
+console.log(printLine(l3));
+
+document.getElementById("line1").innerHTML = printLine(l1);
+document.getElementById("line2").innerHTML = printLine(l2);
+document.getElementById("line3").innerHTML = printLine(l3);
+
+function makeline(syl,max) {
+  let r = [];
+  while(sylNum<syl){
+    randNext = getRandomInt(0,max);
+    currentType = posNext[currentType][randNext];
+    do{
+      word = (posArr[currentType][getRandomInt(0,posNumArr[currentType])]);
+      newSyl = syllable(word);
+    }while((newSyl+sylNum)>syl);
+    r.push(word);
+    sylNum+=newSyl;
+  };
+  sylNum = 0;
+  return r;
 }
 
-//generates pos for line2 words 1 and 2
-currentType = nextType;
-randNext = getRandomInt(0,3);
-currentType = posNext[currentType][randNext];
-//console.log(currentType);
-randNext = getRandomInt(0,3);
-nextType = posNext[currentType][randNext];
-//console.log(nextType);
-
-//generates pos for line2 word3
-randNext = getRandomInt(0,3);
-nextType2 = posNext[nextType][randNext];
-//console.log(nextType2);
-
-//generates words for line2
-while(sylCt !=7){
-    word3 = posArr[currentType][getRandomInt(0,posNumArr[currentType])];
-    word4 = posArr[nextType][getRandomInt(0,posNumArr[nextType])];
-    word5 = posArr[nextType2][getRandomInt(0,posNumArr[nextType2])];
-    line2 = word3 +" "+ word4 +" "+ word5 ;
-    sylCt = syllable(line2);
+function printLine(line){
+  let str="";
+  for(let i = 0; i < line.length; i++)
+    str += line[i]+" ";
+  return str;
 }
-
-//generates pos for line3
-currentType = nextType2;
-randNext = getRandomInt(0,3);
-currentType = posNext[currentType][randNext];
-//console.log(currentType);
-randNext = getRandomInt(0,3);
-nextType = posNext[currentType][randNext];
-//console.log(nextType);
-
-//generates words for line3
-while(sylCt !=5){
-    word6 = posArr[currentType][getRandomInt(0,posNumArr[currentType])];
-    word7 = posArr[nextType][getRandomInt(0,posNumArr[nextType])];
-    line3 = word6 +" "+ word7;
-    sylCt = syllable(line3);
-
-}
-
-//displays haiku
-console.log(line1+"\n"+line2+"\n"+line3);
-let haiku = line1+"\n"+line2+"\n"+line3
-document.getElementById("haiku").innerHTML = haiku
-
 
 //takes in min and max and generates random value
 function getRandomInt(min, max) {
